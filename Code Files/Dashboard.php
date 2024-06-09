a01c5b20067e3f0927582e82ae4c59ad371736c1
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-06-09 12:48:29 +0530

    Update Dashboard.php

--- Code Files/Dashboard.php
+++ Code Files/Dashboard.php
@@ -172,7 +172,7 @@ $conn->close();
         </div>
     </div>
 
-    <!-- =========== Scripts ========= -->
+<!--     Icons Script -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
