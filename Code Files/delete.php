23756d9d1e92d53d31ed960492fd7624b5320afc
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-06-20 12:19:58 +0530

    Update delete.php

--- Code Files/delete.php
+++ Code Files/delete.php
@@ -1,10 +1,20 @@
 <?php
+// Getting the Id
 $id = $_GET['id'];
+
+
+// Database Details
 $con = mysqli_connect("127.0.0.1:3308","root",'',"envirosphere");
+
+// Delete Query
 $sql = "DELETE FROM `images` WHERE `id`='$id'";
+
+// Result
 $result = mysqli_query($con, $sql);
 if($result)
 {
+
+    // Redirection
     header("location: yt.php");
 }
 
