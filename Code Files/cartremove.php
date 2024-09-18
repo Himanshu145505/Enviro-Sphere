deb281cce72f7648c30092308c8e7e306967c314
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-09-18 19:20:02 +0530

    Update cartremove.php

--- Code Files/cartremove.php
+++ Code Files/cartremove.php
@@ -3,8 +3,7 @@
 $id = $_GET['id'];
 // Connection Details
 
-// default Server Name
-$con = mysqli_connect("","root",'',"envirosphere");
+$con = mysqli_connect("127.0.0.1:3308","root",'',"envirosphere");
 // SQL Query for Deleteing the Product from the Cart
 $sql = "DELETE FROM `cart` WHERE `id`='$id'";
 $result = mysqli_query($con, $sql);
@@ -13,5 +12,3 @@ if($result)
     // Redirection After Removing the Product
     header("location: shop.php");
 }
-
-?>
