ac4fbfcd3f3973e77222ee80dab3628bddbefd96
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-09-26 19:36:28 +0530

    Update placeorder.php

--- Code Files/placeorder.php
+++ Code Files/placeorder.php
@@ -18,23 +18,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Check if the "Place Order" button is clicked
-if (isset($_POST['place_order'])) {
-    // Get the current date and time
-    $date_added = date('Y-m-d H:i:s');
-
-    // Get the cart items from the session
-    $cart_items = $_SESSION['cart'];
-
-    // Loop through each cart item and insert into the "orders" table
-    // It will fetch the item details from cart items
-    foreach ($cart_items as $item) {
-        $name = $item['name'];
-        $description = $item['description'];
-        $price = $item['price'];
-        $image = $item['image'];
-
         // SQL query to insert into the "orders" table
         // When user will click on the Place Order Button then all the items from the cart will be stored into the Orders Table 
         $sql = "INSERT INTO orders (name, description, price, image, date_added) VALUES ('$name', '$description', '$price', '$image', '$date_added')";
